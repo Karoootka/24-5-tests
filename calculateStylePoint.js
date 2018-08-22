9fc5110a7ef16f03364c07fd1c0e1953b758935e
Karolina Madej <kkarolinamadej@gmail.com>
2018-08-22 16:23:53 +0200

    Add function calculating style points

--- calculateStylePoint.js
+++ calculateStylePoint.js
@@ -1,5 +1,13 @@
 const calculateStylePoints = (styleNotes) => {
-
+  let sum;
+  if (styleNotes.length === 5) {
+    const min = Math.min(...styleNotes);
+    const max = Math.max(...styleNotes);
+    sum = (styleNotes.reduce((acc, curr) => acc + curr, 0)) - min - max;
+    return sum;
+  } else {
+    return `Wrong notes`;
+  }
 }
 
 
