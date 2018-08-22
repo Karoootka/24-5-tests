d2132ec591db12dede75bf73abb40c571ae0d852
Karolina Madej <kkarolinamadej@gmail.com>
2018-08-22 16:40:13 +0200

    Add function calculating distance points

--- calculateDistancePoints.js
+++ calculateDistancePoints.js
@@ -1,5 +1,22 @@
 const calculateDistancePoints = (distance, hillSize, kPoint) => {
+  let points = 60;
 
+  if (hillSize === "normal") {
+    points = points + ((distance - kPoint) * 2);
+    return points;
+  }
+
+  if(hillSize === "big") {
+    points = points + ((distance - kPoint) * 1.8);
+    return points;
+  }
+
+  if(hillSize === "mamooth") {
+    points = 2*points + ((distance - kPoint) * 1.2);
+    return points;
+  }
+
+  return 'Wrong hill size name';
 }
 
 
